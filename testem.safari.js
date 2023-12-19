d5bee858977ee0118ed1cd96d586822e24af79cb
Sander Knauff <sanderknauff@hotmail.com>
2023-12-19 16:19:12 +0100

    Guide Safari to correct url; Reduce browser start timeout

--- testem.safari.js
+++ testem.safari.js
@@ -5,7 +5,22 @@ module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
   parallel: 1,
-  browser_start_timeout: 2000,
+  browser_start_timeout: 120,
   browser_disconnect_timeout: 120,
   launch_in_ci: ['Safari'],
+  launchers: {
+    // Allow safari to proceed without user intervention. See https://github.com/testem/testem/issues/1387
+    Safari: {
+      protocol: 'browser',
+      exe: 'osascript',
+      args: [
+        '-e',
+        `tell application "Safari"
+                  activate
+                  open location "http://localhost:4200/tests"
+                 end tell
+                 delay 3000`,
+      ],
+    },
+  },
 };
